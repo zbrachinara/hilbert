f48a91a939f430741242bd9295f5c63968bb1139
zbrachinara <zbrachinara@gmail.com>
2025-03-04 15:18:51 -0500

    make point into a type

--- Hilbert/Geometry.lean
+++ Hilbert/Geometry.lean
@@ -1,22 +1,19 @@
 import Hilbert.Set
 
-class Geometry (α : Type) where
-  point : Set α
-  line : Set (Set α)
-  closed: line ⊆ point.power_set
+class Geometry (point : Type) where
+  line : Set (Set point)
 
-class IncidenceGeometry (α : Type) extends Geometry α where
-  unique_line : ∀ x y ∈ point, ∃ l ∈ line, ∀ l' ∈ line, a ∈ l' → b ∈ l' → l = l'
+class IncidenceGeometry (point : Type) extends Geometry point where
+  unique_line : ∀ x y : point, ∃ l ∈ line, ∀ l' ∈ line, x ∈ l' → y ∈ l' → l = l'
   line_nonempty : ∀ l ∈ line, ∃ x y ∈ point, x ≠ y ∧ x ∈ l ∧ y ∈ l
-  non_colinearity : ∃ a b c ∈ point,
-    ∀ l ∈ line, a ∉ l ∨ b ∉ l ∨ c ∉ l
+  non_colinearity : ∃ a b c : point, ∀ l ∈ line, a ∉ l ∨ b ∉ l ∨ c ∉ l
 
-class PointOrder (α : Type) where
-  rel (a b c : α) : Prop
+class PointOrder (point : Type) where
+  between (a b c : point) : Prop
 
 syntax (name := order_relation) "⟪" term " ∗ " term " ∗ " term "⟫": term
 macro_rules
-| `(⟪$p ∗ $q ∗ $r⟫) => `(PointOrder.rel $p $q $r)
+| `(⟪$p ∗ $q ∗ $r⟫) => `(PointOrder.between $p $q $r)
 
-class OrderGeometry (α : Type) [PointOrder α] extends IncidenceGeometry α where
-  order_symmetric : ∀ a b c ∈ point, ⟪a ∗ b ∗ c⟫ → ⟪c ∗ b ∗ a⟫
+class OrderGeometry (point : Type) [PointOrder point] extends IncidenceGeometry point where
+  order_symmetric : ∀ a b c : point, ⟪a ∗ b ∗ c⟫ → ⟪c ∗ b ∗ a⟫
