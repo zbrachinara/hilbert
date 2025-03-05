41b6760c53a9b755cd2e52cdcdf93fc0a0012770
zbrachinara <zbrachinara@gmail.com>
2025-03-05 17:09:06 -0500

    add notation for line separation

--- Hilbert/LineSeparation.lean
+++ Hilbert/LineSeparation.lean
@@ -1,18 +1,24 @@
 import Hilbert.Geometry
 
-open Geometry
-
 def line_sidedness {point} [OrderGeometry point] (l : Line' point) (p q : point) :=
   (segment p q) ∩ l = {}
 
+-- TODO workshop notation, doesn't look good without parentheses
+notation l " ⇇ " x:40 ", " y:40 => line_sidedness l x y
+notation x " ⇇ " l " ⇉ " y:40 => ¬ line_sidedness l x y
+
 theorem line_sidedness_is_equivalence {point} [geo : OrderGeometry point] :
-  ∀ l ∈ geo.line, Equivalence (line_sidedness l) := by sorry
+  ∀ l ∈ geo.line, Equivalence (line_sidedness l) := by
+  intro l l_line
+  constructor
+  · sorry
+  · sorry
+  · sorry
 
 theorem plane_separation {point} [geo : OrderGeometry point] :
-  ∀ l ∈ geo.line, ∀ a b p ∈ Set.every point - l,
-  ¬ line_sidedness l a b → line_sidedness l p a ∨ line_sidedness l p b
+  ∀ l ∈ geo.line, ∀ a b p ∈ Set.every point - l, (a ⇇ l ⇉ b) → (l ⇇ p, a) ∨ (l ⇇ p, b)
   := by sorry
 
 theorem line_separation {point} [geo : OrderGeometry point] : ∀ l ∈ geo.line, ∀ a b c p ∈ l,
-  ⟪a ∗ c ∗ b⟫ → ⟪a ∗ p ∗ c⟫ ∨ ⟪c ∗ p ∗ b⟫
+  ⟪a ∗ c ∗ b⟫ → p ≠ c → ⟪a ∗ p ∗ c⟫ ∨ ⟪c ∗ p ∗ b⟫
   := by sorry
